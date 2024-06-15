40e5dd47c82044a94e559905d83b0ddea0df5fca
Ankit Panigrahi <panigrahi0702@gmail.com>
2024-06-15 20:45:57 +0530

    Now I have added dark mode in the CSS and added JS as well

---
+++ script.js
@@ -0,0 +1,7 @@
+var icon = document.getElementById("icon");
+icon.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        icon.src = "assets/sun.png"
+    }
+}
