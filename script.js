0096b7c5c5c033a703ce577887dbd8d22fb23582
Ankit Panigrahi <panigrahi0702@gmail.com>
2024-06-15 20:53:11 +0530

    Added Animations in switching from dark to light and icons as well

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 var icon = document.getElementById("icon");
+
 icon.onclick = function () {
     document.body.classList.toggle("dark-theme");
-    if (document.body.classList.contains("dark-theme")) {
-        icon.src = "assets/sun.png"
-    }
+
+    setTimeout(function(){
+        if (document.body.classList.contains("dark-theme")) {
+            icon.src = "assets/sun.png";
+        } else {
+            icon.src = "assets/moon.png";
+        }
+    }, 100);
 }
